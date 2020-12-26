829674578285be28747b2004fff887596832361a
ryadav96 <rec.nita@gmail.com>
2020-12-26 14:24:30 +0530

    created file structures of project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Reducers from "../reducers";
 
-import App from "./App";
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
